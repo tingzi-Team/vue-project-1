069bc34f858a494d0964af16a86adf1c316d2a44
tingzi2023 <119830518+tingzi2023@users.noreply.github.com>
2023-02-14 16:50:58 +0800

    Update App.vue
    提交备注信息鞍钢

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to shanghai"/>
+    <HelloWorld msg="Welcome to nanjing"/>
   </div>
 </template>
 
